d58ae2ace39ad5baa563a604b766fe23b1a7bdd0
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-03-03 17:20:03 +0100

    merge develop

--- src/components/CombineImages.jsx
+++ src/components/CombineImages.jsx
@@ -5,24 +5,116 @@ import { UploadedImages } from './UploadedImages'
 import { Dropzone } from './Dropzone'
 
 export const CombineImages = () => {
-  // const uploadImages = async () => {
-  //   const request = new XMLHttpRequest()
-  //   const formData = new FormData()
-  //   request.open('POST', API_URL, true)
-  //   request.onreadystatechange = () => {
-  //     if (request.readyState === 4 && request.status === 200) {
-  //       console.log(JSON.parse(request.response))
-  //     }
-  //   }
-  //   formData.append('upload_preset', 'hackathon')
-  //   formData.append('timestamp', Date.now() / 1000)
-  //   formData.append('api_key', 976248287242189)
-  //   for (let index = 0; index < images.length; index++) {
-  //     const element = images[index]
-  //     formData.append('file', element)
-  //   }
-  //   request.send(formData)
-  // }
+  const [images, setImages] = useState([])
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 2,
+    accept: {
+      'image/jpeg': [],
+      'image/png': [],
+    },
+    onDrop: (acceptedFiles) => {
+      if (
+        images.length >= 2 ||
+        acceptedFiles.length > 2 ||
+        acceptedFiles.length === 0 ||
+        (images.length === 1 && acceptedFiles.length === 2)
+      ) {
+        toast.error('You can only upload 2 images')
+
+        return
+      }
+
+      const acceptedFilesArray = acceptedFiles.map((file) => file.name)
+
+      if (images.find((file) => acceptedFilesArray.includes(file.name))) {
+        toast.error('Cant upload images with the same name')
+
+        return
+      }
+
+      const newFiles = acceptedFiles.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+
+      setImages([...images, ...newFiles])
+    },
+  })
+
+  const style = useMemo(
+    () => ({
+      ...dropZoneStyle,
+    }),
+    []
+  )
+
+  const removeImage = (name) => {
+    const newArray = images.filter((file) => file.name !== name)
+    setImages(newArray)
+  }
+
+  const handleCheck = (e) => {
+    console.log(e.target.value)
+  }
+
+  const previewImages = images.map((file) => (
+    <div style={previewInner} key={file.name}>
+      <img
+        src={file.preview}
+        style={img}
+        onLoad={() => {
+          URL.revokeObjectURL(file.preview)
+        }}
+      />
+      <div className="flex flex-row w-full justify-center gap-8">
+        <input
+          className="w-4"
+          type="radio"
+          name="radio"
+          value={file.name}
+          onChange={handleCheck}
+        />
+        <button
+          className="bg-red-600 text-white p-1 rounded-md"
+          onClick={() => {
+            removeImage(file.name)
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ))
+
+  const updateImage = (imageId, imageName) => {
+    const image = images.filter((image) => image.name === imageName)
+    console.log(image)
+  }
+
+  const uploadImages = async () => {
+    for (let i = 0; i < images.length; i++) {
+      const formData = new FormData()
+      const image = images[i]
+
+      formData.append('file', image)
+      formData.append('upload_preset', 'hackathon')
+      formData.append('timestamp', Date.now() / 1000)
+      // this is not the secrete api key
+      formData.append('api_key', 976248287242189)
+
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        body: formData,
+      })
+
+      const data = await response.json()
+      const { format, original_filename, public_id: imageId } = data
+      const imageName = original_filename + '.' + format
+
+      updateImage(imageId, imageName)
+    }
+  }
 
   return (
     <>
