e83a30074c917d4fc35cb1927d6b78ac81609705
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-03-04 18:51:34 +0100

    Adding new constants

--- src/consts.js
+++ src/consts.js
@@ -7,12 +7,50 @@ export const POSITIONS = [
   { value: 'TOP_RIGHT', label: 'Top right' },
   { value: 'CENTER_LEFT', label: 'Center left' },
   { value: 'CENTER', label: 'Center' },
-  { value: 'CENTER_RIGHT', label: 'Center left' },
+  { value: 'CENTER_RIGHT', label: 'Center right' },
   { value: 'BOTTOM_LEFT', label: 'Bottom left' },
-  { value: 'BOTTOM_CENTER', label: 'Bottom left' },
-  { value: 'BOTTOM_RIGHT', label: 'Bottom left' },
+  { value: 'BOTTOM_CENTER', label: 'Bottom center' },
+  { value: 'BOTTOM_RIGHT', label: 'Bottom right' },
 ]
 
 export const STYLES = [
   { value: '', label: 'Select an option' },
+  { value: 'FADED', label: 'Faded' },
 ]
+
+export const SIZES = [
+  { value: '', label: 'Select an option' },
+  { value: 'SMALL', label: 'Small' },
+  { value: 'MEDIUM', label: 'Medium' },
+  { value: 'BIG', label: 'Big' },
+]
+
+export const INPUT_DEFAULT = {
+  image: {},
+  texts: [],
+  items: [],
+}
+
+export const SIZE_OFFSET = {
+  SMALL: 20,
+  MEDIUM: 35,
+  BIG: 50,
+}
+
+export const IMAGE_SIZE = {
+  SMALL: 75,
+  MEDIUM: 100,
+  BIG: 125,
+}
+
+export const OFFSET_SIGN = {
+  TOP_LEFT: { x: '-', y: '-' },
+  TOP_CENTER: { x: '', y: '-' },
+  TOP_RIGHT: { x: '', y: '-' },
+  CENTER_LEFT: { x: '-', y: '' },
+  CENTER: { x: '', y: '' },
+  CENTER_RIGHT: { x: '', y: '' },
+  BOTTOM_LEFT: { x: '-', y: '' },
+  BOTTOM_CENTER: { x: '', y: '' },
+  BOTTOM_RIGHT: { x: '', y: '' },
+}
