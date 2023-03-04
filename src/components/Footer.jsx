65457693c8c2ffd0688e0647fd80467a73784aee
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-03-04 16:15:19 +0100

    Adding margins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import cloudinaryLogo from '../assets/cloudinaryLogo.svg'
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-center item-center gap-x-2 font-semibold pt-10">
+    <footer className="flex justify-center item-center gap-x-2 font-semibold mt-20">
       Made with{' '}
       <a href="https://cloudinary.com/" target="_blank" rel="noreferrer">
         <img className="w-36" src={cloudinaryLogo} alt="Cloudinary Logo" />
