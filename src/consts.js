e6b220370c76f6174ade817bfbd1e7a3c48c46d9
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-03-04 14:20:28 +0100

    New constants

--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,18 @@
-export const API_URL='https://api.cloudinary.com/v1_1/totisama/image/upload'
+export const API_URL = 'https://api.cloudinary.com/v1_1/totisama/image/upload'
+
+export const POSITIONS = [
+  { value: '', label: 'Select an option' },
+  { value: 'TOP_LEFT', label: 'Top left' },
+  { value: 'TOP_CENTER', label: 'Top center' },
+  { value: 'TOP_RIGHT', label: 'Top right' },
+  { value: 'CENTER_LEFT', label: 'Center left' },
+  { value: 'CENTER', label: 'Center' },
+  { value: 'CENTER_RIGHT', label: 'Center left' },
+  { value: 'BOTTOM_LEFT', label: 'Bottom left' },
+  { value: 'BOTTOM_CENTER', label: 'Bottom left' },
+  { value: 'BOTTOM_RIGHT', label: 'Bottom left' },
+]
+
+export const STYLES = [
+  { value: '', label: 'Select an option' },
+]
